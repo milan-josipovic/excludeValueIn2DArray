df226829bfbc2f230497897222c61c02a00cdf37
milan-josipovic <milan.josipovic@protonmail.com>
2022-02-16 11:44:55 +0100

    Solution

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+
     public static void main(String[] args) {
 
+        CLEANING: for(char stables = 'a'; stables<='d'; stables++) {
+                     for(int leopard = 1; leopard<4; leopard++) {
+                             if(stables=='b' || leopard==2) {
+                                 // no continue
+//                                 continue;
+//                                 continue CLEANING;
+                                 }
+                             System.out.println("Cleaning: "+stables+","+leopard);
+                     }
+                    }
     }
 }
